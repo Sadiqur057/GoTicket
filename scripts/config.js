ec3ad396ab86bf2879f80a6193fbd69ba712490e
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-02-18 03:10:19 +0600

    configured classes for common color

---
+++ scripts/config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        '14': 'repeat(14, minmax(0, 1fr))',
+      },
+      colors: {
+        clifford: '#da373d',
+        'c-primary': '#1dd100',
+        'c-secondary': '#03071299',
+        'c-heading': '#030712',
+        'c-seat': '#f7f8f8',
+      }
+    }
+  }
+}
